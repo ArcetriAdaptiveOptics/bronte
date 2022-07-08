5b42b81aa8bf2140f37bf771a7f9698cee591750
Lorenzo Busoni <lbusoni@gmail.com>
2022-07-08 13:25:42 +0200

    startup

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='bronte',
+      description='Bronte - the single eye MCAO system',
+      version='0.1',
+      classifiers=['Development Status :: 4 - Beta',
+                   'Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python :: 3',
+                   ],
+      long_description=open('README.md').read(),
+      url='',
+      author_email='lorenzo.busoni@inaf.it',
+      author='Arcetri Adaptive Optics team',
+      license='',
+      keywords='adaptive optics',
+      packages=['bronte',
+                ],
+      install_requires=["numpy",
+                        ],
+      package_data={
+          'bronte': ['data/*'],
+      },
+      include_package_data=True,
+      test_suite='test',
+      )
