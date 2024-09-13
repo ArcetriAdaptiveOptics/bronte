876af0846a7ac32c9949d8ef10963ad3ca6cf783
idi92 <edoardo.bellone@inaf.it>
2024-09-13 17:59:38 +0200

    Modified estimation of 3sigma for time rise and fall estimation, to be continued...

--- bronte/mains/main240911rise_time_analysis.py
+++ bronte/mains/main240911rise_time_analysis.py
@@ -21,6 +21,8 @@ class PhotodiodeDataAnalyzer():
         
         self._fname_blink_pistons = self.FDIR + "20240906_1523_blink_loop1_dwell100ms_pistons\\Analog - 9-6-2024 3-23-56.02487 PM.csv"
         
+        self._fname_pd2_on_axis = self.FDIR + "20240912_1622\\Analog - 9-12-2024 4-22-40.67322 PM.csv"
+        
     def fix_command_on_pds(self):
         
         rta_pd1 = ResponseTimeAnalyzer(self._fname_fix_on_pd1, self._fname_bkg)
@@ -61,13 +63,17 @@ class PhotodiodeDataAnalyzer():
 
     def plico_data(self):
         
-        rta_plico = ResponseTimeAnalyzer(self._fname_plico, self._fname_bkg)
-        display_rise_time_analysis_of_plicodata20240906_1232(rta_plico)
+        rta_plico = ResponseTimeAnalyzer(self._fname_plico)
+        return display_rise_time_analysis_of_plicodata20240906_1232(rta_plico)
 
     def blink_data_dwell_time0ms(self):
         
-        rta_blink = ResponseTimeAnalyzer(self._fname_blink_dwt0ms, self._fname_bkg)
-        display_rise_time_analysis_of_data20240906_1422(rta_blink)
+        rta_blink = ResponseTimeAnalyzer(self._fname_blink_dwt0ms)
+        return display_rise_time_analysis_of_data20240906_1422(rta_blink)
+    
+    def plico_data_pd2_on_axis(self):
+        rta_plico_pd2_on_axis = ResponseTimeAnalyzer(self._fname_pd2_on_axis)
+        return display_rise_time_analysis_of_plicodata20240912_1622(rta_plico_pd2_on_axis)
         
     def blink_data_dwell_time100ms_pistons(self):
         
@@ -76,22 +82,155 @@ class PhotodiodeDataAnalyzer():
         
     
 
+def display_rise_time_analysis_of_plicodata20240912_1622(rta):
+    
+    d_index = np.arange(0, 100200)
+    vd = rta._v1_sma[d_index].mean()
+    abs_err_vd = np.abs(rta._v1_sma[d_index] - rta._v1[d_index])
+    mae_vd = np.mean(abs_err_vd)
+    err_vd = 3 * rta._v1_sma[d_index].std()#3 * mae_vd
+    
+    c_index = np.arange(100750, 104043)
+    vc = rta._v2_sma[c_index].mean()
+    abs_err_vc = np.abs(rta._v2_sma[c_index] - rta._v2[c_index])
+    mae_vc = np.mean(abs_err_vc)
+    err_vc = 3 * rta._v2_sma[c_index].std()#3 * mae_vc
+    
+    b_index = np.arange(100725, 104111)
+    vb = rta._v1_sma[b_index].mean()
+    abs_err_vb = np.abs(rta._v1_sma[b_index]-rta._v1[b_index])
+    mae_vb = np.mean(abs_err_vb)
+    err_vb = 3 * rta._v1_sma[b_index].std() #3 * mae_vb
+    
+    idx0 = np.arange(95683, 100170)
+    va0 = rta._v2_sma[idx0].mean()
+    abs_err_va0 = np.abs(rta._v2_sma[idx0] - rta._v2[idx0])
+    mae_va0 = np.mean(abs_err_va0)
+    err_va0 = 3 * rta._v2_sma[idx0].std()#3 * mae_va0
+    
+    idx1 = np.arange(104483, 109085)
+    va1 = rta._v2_sma[idx1].mean()
+    abs_err_va1 = np.abs(rta._v2_sma[idx1] - rta._v2[idx1])
+    mae_va1 = np.mean(abs_err_va1)
+    err_va1 = 3 * rta._v2_sma[idx1].std()#3 * mae_va1
+    
+    idx2 = np.arange(112764, 117578)
+    va2 = rta._v2_sma[idx2].mean()
+    abs_err_va2 = np.abs(rta._v2_sma[idx2] - rta._v2[idx2])
+    mae_va2 = np.mean(abs_err_va2)
+    err_va2 = 3 * rta._v2_sma[idx2].std()#3 * mae_va2
+    
+    idx3 = np.arange(121486, 126226)
+    va3 = rta._v2_sma[idx3].mean()
+    abs_err_va3 = np.abs(rta._v2_sma[idx3] - rta._v2[idx3])
+    mae_va3 = np.mean(abs_err_va3)
+    err_va3 = 3 * rta._v2_sma[idx3].std() #3 * mae_va3
+    
+    idx4 = np.arange(129887, 134926)
+    va4 = rta._v2_sma[idx4].mean()
+    abs_err_va4 = np.abs(rta._v2_sma[idx4] - rta._v2[idx4])
+    mae_va4 = np.mean(abs_err_va4)
+    err_va4 = 3 * rta._v2_sma[idx4].std()#3 * mae_va4
+    
+    idx5 = np.arange(138550, 143404)
+    va5 = rta._v2_sma[idx5].mean()
+    abs_err_va5 = np.abs(rta._v2_sma[idx5] - rta._v2[idx5])
+    mae_va5 = np.mean(abs_err_va5)
+    err_va5 = 3 * rta._v2_sma[idx5].std() #3 * mae_va5
+    
+    idx6 = np.arange(147507,152332)
+    va6 = rta._v2_sma[idx6].mean()
+    abs_err_va6 = np.abs(rta._v2_sma[idx6] - rta._v2[idx6])
+    mae_va6 = np.mean(abs_err_va6)
+    err_va6 = 3 * rta._v2_sma[idx6].std()#3 * mae_va6
+    
+    idx7 = np.arange(156000,160600)
+    va7 = rta._v2_sma[idx7].mean()
+    abs_err_va7 = np.abs(rta._v2_sma[idx7] - rta._v2[idx7])
+    mae_va7 = np.mean(abs_err_va7)
+    err_va7 = 3 * rta._v2_sma[idx7].std()#3 * mae_va7
+    
+    idx8 = np.arange(164449,169053)
+    va8 = rta._v2_sma[idx8].mean()
+    abs_err_va8 = np.abs(rta._v2_sma[idx8] - rta._v2[idx8])
+    mae_va8 = np.mean(abs_err_va8)
+    err_va8 = 3 * rta._v2_sma[idx8].std() #3 * mae_va8
+    
+    idx9 = np.arange(173219,178258)
+    va9 = rta._v2_sma[idx9].mean()
+    abs_err_va9 = np.abs(rta._v2_sma[idx9] - rta._v2[idx9])
+    mae_va9 = np.mean(abs_err_va9)
+    err_va9 = 3 * rta._v2_sma[idx9].std()#3 * mae_va9
+    
+    #rta.display_data_indexing()
+    rta.display_moving_average_signals()
+    
+    plt.hlines(vd, 0, rta._t.max(), ls = '-', colors ='cyan')
+    plt.hlines(vd + err_vd, 0, rta._t.max(), ls = '--', colors ='cyan')
+    
+    plt.hlines(vc, 0, rta._t.max(), ls = '-', colors ='black')
+    plt.hlines(vc + err_vc, 0, rta._t.max(), ls = '--', colors ='black')
+    
+    plt.hlines(vb, 0, rta._t.max(), ls = '-', colors ='cyan')
+    plt.hlines(vb - err_vb, 0, rta._t.max(), ls = '--', colors ='cyan')
+    
+    dindx  = 500
+    
+    plt.hlines(va0, rta._t[idx0.min()- dindx], rta._t[idx0.max()+dindx], ls='-',colors='black')
+    plt.hlines(va0-err_va0, rta._t[idx0.min()- dindx], rta._t[idx0.max()+dindx], ls='--',colors='black')
+    
+    plt.hlines(va1, rta._t[idx1.min()- dindx], rta._t[idx1.max()+dindx], ls='-',colors='black')
+    plt.hlines(va1-err_va1, rta._t[idx1.min()- dindx], rta._t[idx1.max()+dindx], ls='--',colors='black')
+    
+    plt.hlines(va2, rta._t[idx2.min()- dindx], rta._t[idx2.max()+dindx], ls='-',colors='black')
+    plt.hlines(va2-err_va2, rta._t[idx2.min()- dindx], rta._t[idx2.max()+dindx], ls='--',colors='black')
+    
+    plt.hlines(va3, rta._t[idx3.min()- dindx], rta._t[idx3.max()+dindx], ls='-',colors='black')
+    plt.hlines(va3-err_va3, rta._t[idx3.min()- dindx], rta._t[idx3.max()+dindx], ls='--',colors='black')
+    
+    plt.hlines(va4, rta._t[idx4.min()- dindx], rta._t[idx4.max()+dindx], ls='-',colors='black')
+    plt.hlines(va4-err_va4, rta._t[idx4.min()- dindx], rta._t[idx4.max()+dindx], ls='--',colors='black')
+    
+    plt.hlines(va5, rta._t[idx5.min()- dindx], rta._t[idx5.max()+dindx], ls='-',colors='black')
+    plt.hlines(va5-err_va5, rta._t[idx5.min()- dindx], rta._t[idx5.max()+dindx], ls='--',colors='black')
+    
+    plt.hlines(va6, rta._t[idx6.min()- dindx], rta._t[idx6.max()+dindx], ls='-',colors='black')
+    plt.hlines(va6-err_va6, rta._t[idx6.min()- dindx], rta._t[idx6.max()+dindx], ls='--',colors='black')
+    
+    plt.hlines(va7, rta._t[idx7.min()- dindx], rta._t[idx7.max()+dindx], ls='-',colors='black')
+    plt.hlines(va7-err_va7, rta._t[idx7.min()- dindx], rta._t[idx7.max()+dindx], ls='--',colors='black')
+    
+    plt.hlines(va8, rta._t[idx8.min()- dindx], rta._t[idx8.max()+dindx], ls='-',colors='black')
+    plt.hlines(va8-err_va8, rta._t[idx8.min()- dindx], rta._t[idx8.max()+dindx], ls='--',colors='black')
+    
+    plt.hlines(va9, rta._t[idx9.min()- dindx], rta._t[idx9.max()+dindx], ls='-',colors='black')
+    plt.hlines(va9-err_va9, rta._t[idx9.min()- dindx], rta._t[idx9.max()+dindx], ls='--',colors='black')
+    
 
-
+    return (va0, err_va0, mae_va0), (vb, err_vb, mae_vb), (vc, err_vc, mae_vc), (vd, err_vd, mae_vd)
 
 def display_rise_time_analysis_of_plicodata20240906_1232(rta):
     
     # fname_plico = "C:\\Users\\labot\\Desktop\\misure_tesi_slm\\slm_time_response\\photodiode\\20240906_1232_1\\Analog - 9-6-2024 12-32-43.52892 PM.csv"
     # rta = ResponseTimeAnalyzer(fname_plico)
     
-    c_index = np.arange(0, 119800)
+    d_index = np.arange(0, 119800)
+    vd = rta._v2_sma[d_index].mean()
+    abs_err_vd = np.abs(rta._v2_sma[d_index]-rta._v2[d_index])
+    mae_vd = np.mean(abs_err_vd)
+    err_vd = 3 * rta._v2_sma[d_index].std() #3 * mae_vd #0.1 * vd
+    
+    c_index = np.arange(205500,len(rta._v1_sma))
+    vc = rta._v1_sma[c_index].mean()
+    abs_err_vc = np.abs(rta._v1_sma[c_index]-rta._v1[c_index])
+    mae_vc = np.mean(abs_err_vc)
+    err_vc = 3 * rta._v1_sma[c_index].std() #3 * mae_vc #0.1 * vc
+    
     b_index = np.arange(205000, len(rta._v2_sma))
-    
-    vc = rta._v2_sma[c_index].mean()
-    err_vc = 0.1 * vc
-    
     vb = rta._v2_sma[b_index].mean()
-    err_vb = 0.1 * vb
+    abs_err_vb = np.abs(rta._v2_sma[b_index] - rta._v2[b_index])
+    mae_vb = np.mean(abs_err_vb)  
+    err_vb = 3 * rta._v2_sma[b_index].std() #3 * mae_vb #0.1 * vb
     
     idx0 = np.arange(116649, 121943)
     idx1 = np.arange(125515, 130920)
@@ -105,80 +244,113 @@ def display_rise_time_analysis_of_plicodata20240906_1232(rta):
     idx9 = np.arange(199133, 204419)
     
     va0 = rta._v1_sma[idx0].mean()
-    err_va0 = 0.1 * va0
-    va1 = rta._v1_sma[idx1].mean()
-    err_va1 = 0.1 * va1
-    va2 = rta._v1_sma[idx2].mean()
-    err_va2 = 0.1 * va2
-    va3 = rta._v1_sma[idx3].mean()
-    err_va3 = 0.1 * va3
-    va4 = rta._v1_sma[idx4].mean()
-    err_va4 = 0.1 * va4
-    va5 = rta._v1_sma[idx5].mean()
-    err_va5 = 0.1 * va5
-    va6 = rta._v1_sma[idx6].mean()
-    err_va6 = 0.1 * va6
-    va7 = rta._v1_sma[idx7].mean()
-    err_va7 = 0.1 * va7
-    va8 = rta._v1_sma[idx8].mean()
-    err_va8= 0.1 * va8
-    va9 = rta._v1_sma[idx9].mean()
-    err_va9 = 0.1 * va9
+    abs_err_va0 = np.abs(rta._v1_sma[idx0]-rta._v1[idx0])
+    mae_va0 = np.mean(abs_err_va0)
+    err_va0 = 3 * rta._v1_sma[idx0].std()#3 * mae_va0 # 0.1 * va0
     
+    va1 = rta._v1_sma[idx1].mean()
+    abs_err_va1 = np.abs(rta._v1_sma[idx1]-rta._v1[idx1])
+    mae_va1 = np.mean(abs_err_va1)
+    err_va1 = 3 * rta._v1_sma[idx1].std() #3 * mae_va1 #0.1 * va1
+    
+    va2 = rta._v1_sma[idx2].mean()
+    abs_err_va2 = np.abs(rta._v1_sma[idx2]-rta._v1[idx2])
+    mae_va2 = np.mean(abs_err_va2)
+    err_va2 = 3 * rta._v1_sma[idx2].std() #3 * mae_va2 #0.1 * va2
+    
+    va3 = rta._v1_sma[idx3].mean()
+    abs_err_va3 = np.abs(rta._v1_sma[idx3]-rta._v1[idx3])
+    mae_va3 = np.mean(abs_err_va3)
+    err_va3 = 3 * rta._v1_sma[idx3].std() #3 * mae_va3 #0.1 * va3
+    
+    va4 = rta._v1_sma[idx4].mean()
+    abs_err_va4 = np.abs(rta._v1_sma[idx4]-rta._v1[idx4])
+    mae_va4 = np.mean(abs_err_va4)
+    err_va4 = 3 * rta._v1_sma[idx4].std()#3 * mae_va4 #0.1 * va4
+    
+    va5 = rta._v1_sma[idx5].mean()
+    abs_err_va5 = np.abs(rta._v1_sma[idx5]-rta._v1[idx5])
+    mae_va5 = np.mean(abs_err_va5)
+    err_va5 = 3 * rta._v1_sma[idx5].std() #3 * mae_va5 #0.1 * va5
+    
+    va6 = rta._v1_sma[idx6].mean()
+    abs_err_va6 = np.abs(rta._v1_sma[idx6]-rta._v1[idx6])
+    mae_va6 = np.mean(abs_err_va6)
+    err_va6 = 3 * rta._v1_sma[idx6].std() #3 * mae_va6 #0.1 * va6
+    
+    va7 = rta._v1_sma[idx7].mean()
+    abs_err_va7 = np.abs(rta._v1_sma[idx7]-rta._v1[idx7])
+    mae_va7 = np.mean(abs_err_va7)
+    err_va7 = 3 * rta._v1_sma[idx7].std() #3 * mae_va7 #0.1 * va7
+    
+    va8 = rta._v1_sma[idx8].mean()
+    abs_err_va8 = np.abs(rta._v1_sma[idx8]-rta._v1[idx8])
+    mae_va8 = np.mean(abs_err_va8)
+    err_va8 = 3 * rta._v1_sma[idx8].std() #3 * mae_va8 #0.1 * va8
+    
+    va9 = rta._v1_sma[idx9].mean()
+    abs_err_va9 = np.abs(rta._v1_sma[idx9]-rta._v1[idx9])
+    mae_va9 = np.mean(abs_err_va9)
+    err_va9 = 3 * rta._v1_sma[idx9].std()#3 * mae_va9 #0.1 * va9
+    
+    #rta.display_data_indexing()
     rta.display_moving_average_signals()
     plt.title("Plico data 240906_1231")
     
-    plt.hlines(vc, 0 , rta._t.max(), ls='-',colors='black')
-    plt.hlines(vc+err_vc, 0 , rta._t.max(), ls='--',colors='black')
-    plt.hlines(vc-err_vc, 0 , rta._t.max(), ls='--',colors='black')
+    plt.hlines(vd, 0 , rta._t.max(), ls='-',colors='black')
+    plt.hlines(vd+err_vd, 0 , rta._t.max(), ls='--',colors='black')
+    #plt.hlines(vd-err_vd, 0 , rta._t.max(), ls='--',colors='black')
+    
+    plt.hlines(vc, 0, rta._t.max(), ls='-', colors='cyan')
+    plt.hlines(vc+err_vc, 0, rta._t.max(), ls='--', colors='cyan')
     
     plt.hlines(vb, 0 , rta._t.max(), ls='-',colors='black')
-    plt.hlines(vb+err_vb, 0 , rta._t.max(), ls='--',colors='black')
+    #plt.hlines(vb+err_vb, 0 , rta._t.max(), ls='--',colors='black')
     plt.hlines(vb-err_vb, 0 , rta._t.max(), ls='--',colors='black')
     
     dindx  = 500
     
     plt.hlines(va0, rta._t[idx0.min()- dindx], rta._t[idx0.max()+dindx], ls='-',colors='cyan')
-    plt.hlines(va0+err_va0, rta._t[idx0.min()- dindx], rta._t[idx0.max()+dindx], ls='--',colors='cyan')
+    #plt.hlines(va0+err_va0, rta._t[idx0.min()- dindx], rta._t[idx0.max()+dindx], ls='--',colors='cyan')
     plt.hlines(va0-err_va0, rta._t[idx0.min()- dindx], rta._t[idx0.max()+dindx], ls='--',colors='cyan')
     
     plt.hlines(va1, rta._t[idx1.min()- dindx], rta._t[idx1.max()+dindx], ls='-',colors='cyan')
-    plt.hlines(va1+err_va1, rta._t[idx1.min()- dindx], rta._t[idx1.max()+dindx], ls='--',colors='cyan')
+    #plt.hlines(va1+err_va1, rta._t[idx1.min()- dindx], rta._t[idx1.max()+dindx], ls='--',colors='cyan')
     plt.hlines(va1-err_va1, rta._t[idx1.min()- dindx], rta._t[idx1.max()+dindx], ls='--',colors='cyan')
     
     plt.hlines(va2, rta._t[idx2.min()- dindx], rta._t[idx2.max()+dindx], ls='-',colors='cyan')
-    plt.hlines(va2+err_va2, rta._t[idx2.min()- dindx], rta._t[idx2.max()+dindx], ls='--',colors='cyan')
+    #plt.hlines(va2+err_va2, rta._t[idx2.min()- dindx], rta._t[idx2.max()+dindx], ls='--',colors='cyan')
     plt.hlines(va2-err_va2, rta._t[idx2.min()- dindx], rta._t[idx2.max()+dindx], ls='--',colors='cyan')
     
     plt.hlines(va3, rta._t[idx3.min()- dindx], rta._t[idx3.max()+dindx], ls='-',colors='cyan')
-    plt.hlines(va3+err_va3, rta._t[idx3.min()- dindx], rta._t[idx3.max()+dindx], ls='--',colors='cyan')
+    #plt.hlines(va3+err_va3, rta._t[idx3.min()- dindx], rta._t[idx3.max()+dindx], ls='--',colors='cyan')
     plt.hlines(va3-err_va3, rta._t[idx3.min()- dindx], rta._t[idx3.max()+dindx], ls='--',colors='cyan')
     
     plt.hlines(va4, rta._t[idx4.min()- dindx], rta._t[idx4.max()+dindx], ls='-',colors='cyan')
-    plt.hlines(va4+err_va4, rta._t[idx4.min()- dindx], rta._t[idx4.max()+dindx], ls='--',colors='cyan')
+    #plt.hlines(va4+err_va4, rta._t[idx4.min()- dindx], rta._t[idx4.max()+dindx], ls='--',colors='cyan')
     plt.hlines(va4-err_va4, rta._t[idx4.min()- dindx], rta._t[idx4.max()+dindx], ls='--',colors='cyan')
     
     plt.hlines(va5, rta._t[idx5.min()- dindx], rta._t[idx5.max()+dindx], ls='-',colors='cyan')
-    plt.hlines(va5+err_va5, rta._t[idx5.min()- dindx], rta._t[idx5.max()+dindx], ls='--',colors='cyan')
+    #plt.hlines(va5+err_va5, rta._t[idx5.min()- dindx], rta._t[idx5.max()+dindx], ls='--',colors='cyan')
     plt.hlines(va5-err_va5, rta._t[idx5.min()- dindx], rta._t[idx5.max()+dindx], ls='--',colors='cyan')
     
     plt.hlines(va6, rta._t[idx6.min()- dindx], rta._t[idx6.max()+dindx], ls='-',colors='cyan')
-    plt.hlines(va6+err_va6, rta._t[idx6.min()- dindx], rta._t[idx6.max()+dindx], ls='--',colors='cyan')
+    #plt.hlines(va6+err_va6, rta._t[idx6.min()- dindx], rta._t[idx6.max()+dindx], ls='--',colors='cyan')
     plt.hlines(va6-err_va6, rta._t[idx6.min()- dindx], rta._t[idx6.max()+dindx], ls='--',colors='cyan')
     
     plt.hlines(va7, rta._t[idx7.min()- dindx], rta._t[idx7.max()+dindx], ls='-',colors='cyan')
-    plt.hlines(va7+err_va7, rta._t[idx7.min()- dindx], rta._t[idx7.max()+dindx], ls='--',colors='cyan')
+    #plt.hlines(va7+err_va7, rta._t[idx7.min()- dindx], rta._t[idx7.max()+dindx], ls='--',colors='cyan')
     plt.hlines(va7-err_va7, rta._t[idx7.min()- dindx], rta._t[idx7.max()+dindx], ls='--',colors='cyan')
     
     plt.hlines(va8, rta._t[idx8.min()- dindx], rta._t[idx8.max()+dindx], ls='-',colors='cyan')
-    plt.hlines(va8+err_va8, rta._t[idx8.min()- dindx], rta._t[idx8.max()+dindx], ls='--',colors='cyan')
+    #plt.hlines(va8+err_va8, rta._t[idx8.min()- dindx], rta._t[idx8.max()+dindx], ls='--',colors='cyan')
     plt.hlines(va8-err_va8, rta._t[idx8.min()- dindx], rta._t[idx8.max()+dindx], ls='--',colors='cyan')
     
     plt.hlines(va9, rta._t[idx9.min()- dindx], rta._t[idx9.max()+dindx], ls='-',colors='cyan')
-    plt.hlines(va9+err_va9, rta._t[idx9.min()- dindx], rta._t[idx9.max()+dindx], ls='--',colors='cyan')
+    #plt.hlines(va9+err_va9, rta._t[idx9.min()- dindx], rta._t[idx9.max()+dindx], ls='--',colors='cyan')
     plt.hlines(va9-err_va9, rta._t[idx9.min()- dindx], rta._t[idx9.max()+dindx], ls='--',colors='cyan')
     
-    
+    return (va0, err_va0, mae_va0), (vb, err_vb, mae_vb), (vc, err_vc, mae_vc), (vd, err_vd, mae_vd)
         
 def display_rise_time_analysis_of_data20240906_1523(rta):
     
@@ -249,35 +421,53 @@ def display_rise_time_analysis_of_data20240906_1523(rta):
         
 def display_rise_time_analysis_of_data20240906_1422(rta):
     
-    c_index = np.arange(0, 49355)
+    d_index = np.arange(0, 49355)
+    vd = rta._v2_sma[d_index].mean()
+    abs_err_vd = np.abs(rta._v2[d_index] - rta._v2_sma[d_index])
+    mae_vd = np.mean(abs_err_vd)
+    err_vd = 3*rta._v2_sma[d_index].std() #3 * mae_vd #0.1 * vd
+    
+    c_index = np.arange(54237, len(rta._v1_sma))
+    vc = rta._v1_sma[c_index].mean()
+    abs_err_vc = np.abs(rta._v1_sma[c_index] - rta._v1[c_index])
+    mae_vc = np.mean(abs_err_vc)
+    err_vc = 3 * rta._v1_sma[c_index].std()#3 * mae_vc #0.1 * vc
+    
     b_index = np.arange(54665, len(rta._v2))
-    a_index = np.arange(0, 49402)
-    
-    va = rta._v1_sma[a_index].mean()
-    err_va = 0.1 * va
-    
     vb = rta._v2_sma[b_index].mean()
-    err_vb = 0.1 * vb
+    abs_err_vb = np.abs(rta._v2[b_index] - rta._v2_sma[b_index])
+    mae_vb = np.mean(abs_err_vb)
+    err_vb = 3 * rta._v2_sma[b_index].std()#3 * mae_vb #0.1 * vb
     
-    vc = rta._v2_sma[c_index].mean()
-    err_vc = 0.1 * vc
+    a_index = np.arange(0, 49402)
+    va = rta._v1_sma[a_index].mean()
+    abs_err_va = np.abs(rta._v1_sma[a_index] - rta._v1[a_index])
+    mae_va = np.mean(abs_err_va)
+    err_va = 3 * rta._v1_sma[a_index].std() #3 * mae_va #0.1 * va
     
+    
+    #rta.display_data_indexing()
     rta.display_moving_average_signals()
     
     plt.title("Blink data 240906_1422 Dwell Time 0ms")
     
     plt.hlines(va, 0, rta._t.max(), ls='-', colors='cyan')
-    plt.hlines(va+err_va, 0, rta._t.max(), ls='--', colors='cyan')
+    #plt.hlines(va+err_va, 0, rta._t.max(), ls='--', colors='cyan')
     plt.hlines(va-err_va, 0, rta._t.max(), ls='--', colors='cyan')
     
     plt.hlines(vb, 0, rta._t.max(), ls='-', colors='black')
-    plt.hlines(vb+err_vb, 0, rta._t.max(), ls='--', colors='black')
+    #plt.hlines(vb+err_vb, 0, rta._t.max(), ls='--', colors='black')
     plt.hlines(vb-err_vb, 0, rta._t.max(), ls='--', colors='black')
     
-    plt.hlines(vc, 0, rta._t.max(), ls='-', colors='black')
-    plt.hlines(vc+err_vc, 0, rta._t.max(), ls='--', colors='black')
-    plt.hlines(vc-err_vc, 0, rta._t.max(), ls='--', colors='black')
-        
+    plt.hlines(vc, 0, rta._t.max(), ls='-', colors='cyan')
+    plt.hlines(vc+err_vc, 0, rta._t.max(), ls='--', colors='cyan')
+    #plt.hlines(vc-err_vc, 0, rta._t.max(), ls='--', colors='cyan')
+    
+    plt.hlines(vd, 0, rta._t.max(), ls='-', colors='black')
+    plt.hlines(vd+err_vd, 0, rta._t.max(), ls='--', colors='black')
+    #plt.hlines(vd-err_vd, 0, rta._t.max(), ls='--', colors='black')
+    
+    return (va, err_va, mae_va), (vb, err_vb, mae_vb), (vc, err_vc, mae_vc), (vd, err_vd, mae_vd)
 
 # def display_rise_time_analysis_of_data20240906_1441():
 #
