base_factory added(to be continued); OpenLoopRunner corrected with wf disturb instead of modal offset;  bronte rtc fixed modal offset subtraction but still to understeand the correct units for the slopes

bronte/mains/main250124_bronte_open_loop_on_modal_offset.py
@@ -1,5 +1,6 @@
 from bronte import startup
 from bronte.mains import main240802_ao_test
+from bronte.telemetry.display_telemetry_data import DisplayTelemetryData
 from arte.types.zernike_coefficients import ZernikeCoefficients
 import numpy as np
      
@@ -11,17 +12,19 @@ class OpenLoopRunner():
         flat = np.zeros(1920*1152)
         self._factory.deformable_mirror.set_shape(flat)
         self._factory.sh_camera.setExposureTime(8)
-        self._zc_offset = None
+        self._zc_distrub = None
         self._tao = main240802_ao_test.TestAoLoop(self._factory)
         self._tao.reset_wavefront_disturb()
         self._tao._factory.pure_integrator_controller._gain = 0
     
-    def set_modal_offset_from_numpy_array(self, modal_offset):
-        self._zc_offset = ZernikeCoefficients.fromNumpyArray(modal_offset)
-        self._tao._factory.rtc.set_modal_offset(self._zc_offset)
-        wf_offset = self._factory.slm_rasterizer.zernike_coefficients_to_raster(self._zc_offset)
-        cmd_offset = self._factory.slm_rasterizer.reshape_map2vector(wf_offset.toNumpyArray())
-        self._factory.deformable_mirror.set_shape(cmd_offset)
+   
+    def set_wavefront_disturb_from_numpy_array(self, modal_coefficients):
+        
+        self._zc_distrub = ZernikeCoefficients.fromNumpyArray(modal_coefficients)
+        wf_disturb = self._factory.slm_rasterizer.zernike_coefficients_to_raster(self._zc_disturb)
+        self._tao._factory.rtc.set_wavefront_disturb(wf_disturb)
+        cmd = self._factory.slm_rasterizer.reshape_map2vector(wf_disturb.toNumpyArray())
+        self._factory.deformable_mirror.set_shape(cmd)
     
     def run(self, steps = 30):
         if self._zc_offset is None:
@@ -33,3 +36,15 @@ class OpenLoopRunner():
         
         self._tao._delete_short_exp_psf()
         self._tao.save_telemetry(ftag)
+        
+
+def main():
+
+    bf = startup.startup()
+    flattening_modal_coefficients, _ = DisplayTelemetryData.load_modal_offset('250203_134800')
+    modal_command = flattening_modal_coefficients.copy()
+    modal_command[0] += 1e-6
+    olr = OpenLoopRunner()
+    olr.set_wavefront_disturb_from_numpy_array(modal_command)
+    olr.run(steps=30)
+    olr.save_telemetry('250203_135500') 