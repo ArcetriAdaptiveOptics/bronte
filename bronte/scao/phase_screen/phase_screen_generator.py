51d24d5f6f2c55f276f8b38a2c917889b6071bd9
idi92 <edoardo.bellone@inaf.it>
2025-04-02 17:09:27 +0200

    trying to improve saving generated phase screen with circular buffers, but fails

--- bronte/scao/phase_screen/phase_screen_generator.py
+++ bronte/scao/phase_screen/phase_screen_generator.py
@@ -4,6 +4,7 @@ from specula import np, cpuArray
 from bronte.startup import set_data_dir
 from bronte.package_data import phase_screen_folder
 from arte.types.wavefront import Wavefront
+from arte.utils.circular_buffer import NumpyCircularBuffer
 from astropy.io import fits 
 import h5py
 
@@ -11,16 +12,18 @@ class PhaseScreenGenerator():
     
     PROPAGATION_DIR = 'on_axis' #'lgs1'
     STORE_PHASE_SCREENS = True
+    BUFFER_SIZE = 100
     
     def __init__(self, scao_factory):
         
         self._factory = scao_factory
         self._slm_raster = self._factory.slm_rasterizer
         self._slm_mask = self._factory.slm_pupil_mask
+        self._slm_mask_diameter = 2*self._slm_mask.radius()
         self._setup_atmosphere()
         self._set_inputs()
         self._define_groups()
-        self._initialise_turbulent_data()
+        self._initialise_telemetry_buffers()
     
 
     def _setup_atmosphere(self):
@@ -39,8 +42,10 @@ class PhaseScreenGenerator():
     
         #self._bench_devices.inputs['ef'].set(self._prop.outputs['out_on_axis_source_ef'])
     
-    def _initialise_turbulent_data(self):
-        self._phase_screen_list = []
+    def _initialise_telemetry_buffers(self):
+
+        frame_shape = (self._slm_mask_diameter, self._slm_mask_diameter)
+        self._phase_screen_buffer = NumpyCircularBuffer(self.BUFFER_SIZE, frame_shape, dtype=np.float16)
         self._modal_coefficients_list = []
     
     def _define_groups(self):
@@ -50,14 +55,16 @@ class PhaseScreenGenerator():
         group3 = [self._prop]
         self._groups = [group1, group2, group3]
         
-    def _update_phase_screen_list(self):
+    def _update_telemetry_buffers(self):
         
         ef_output = 'out_'+self.PROPAGATION_DIR+'_source_ef'
         ef = self._groups[2][0].outputs[ef_output]
         phase_screen = cpuArray(ef.phaseInNm)
         
         if self.STORE_PHASE_SCREENS is True:
-            self._phase_screen_list.append(phase_screen.astype(np.float16)) 
+            self._phase_screen_buffer.store(phase_screen)
+            if self._phase_screen_buffer.counter() == self._phase_screen_buffer._len:
+                self._save_phase_screens_hdf5(self._ftag_hdf5, self._phase_screen_buffer._buf )
         
         phase_screen_on_slm_pupil = self._slm_raster.get_recentered_phase_screen_on_slm_pupil_frame(phase_screen* 1e-9)
         wfz = Wavefront.fromNumpyArray(phase_screen_on_slm_pupil)
@@ -68,9 +75,12 @@ class PhaseScreenGenerator():
         
         self._modal_coefficients_list.append(modal_coefficents.toNumpyArray())
         
-    def run(self, Nsteps = 30, storePhaseScreens = False):
+    def run(self, Nsteps = 30, ftag_pahse_screen_hdf5 = None):
         
-        self.STORE_PHASE_SCREENS = storePhaseScreens
+        if ftag_pahse_screen_hdf5 is not None:
+            self.STORE_PHASE_SCREENS = True
+            self._ftag_hdf5 = ftag_pahse_screen_hdf5
+            
         self._n_steps = Nsteps
         # time step of the simulated loop isnt it QM
         self.time_step = self._factory.TIME_STEP_IN_SEC
@@ -90,8 +100,8 @@ class PhaseScreenGenerator():
                     obj.trigger()
                     obj.post_trigger()
                     
-            self._update_phase_screen_list()
-            
+            self._update_telemetry_buffers()
+                
         for group in self._groups:
             for obj in group:
                 obj.finalize()
@@ -130,19 +140,26 @@ class PhaseScreenGenerator():
     #TODO: MemoryError must be resolved
     # emoryError: Unable to allocate 12.0 GiB
     # for an array with shape (5000, 1136, 1136) and data type float16
+    #TODO: the function must not overwrite but add the data on existing file
     def _save_phase_screens_hdf5(self, ftag, data):
-        frame_sizeY = self._phase_screen_list[0].shape[0]
-        frame_sizeX = self._phase_screen_list[0].shape[1]
+        frame_sizeY = self._slm_mask_diameter
+        frame_sizeX = self._slm_mask_diameter
         file_name = phase_screen_folder() / (ftag + '.h5')
         with h5py.File(file_name, 'w') as f:
             # Create dataset with gzip compression and chunking
             f.create_dataset(
                 'phase_screen', 
-                data=data.astype(np.float16), 
+                data=data.astype(np.float32), 
                 compression='gzip',
                 compression_opts = 9, 
                 chunks=(1, frame_sizeY, frame_sizeX)  # Save frame-wise chunks
             )
+            # # Espandi dataset e salva
+            # f['phase_screen'].resize((f['phase_screen'].shape[0] + self._phase_screen_buffer.counter(),) + f['phase_screen'].shape[1:])
+            # f['phase_screen'][-self._phase_screen_buffer.counter():] = self._phase_screen_buffer._buf
+            # Svuota il buffer dopo il salvataggio
+            self._phase_screen_buffer = NumpyCircularBuffer(self.BUFFER_SIZE, (frame_sizeY, frame_sizeX), dtype=np.float32)
+            
     @staticmethod
     def load_phase_screen_fits_data(ftag):
         set_data_dir()
