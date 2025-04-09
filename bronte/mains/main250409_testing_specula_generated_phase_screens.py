4c07a1492022ed695ee9140fb5321b40d441c20f
idi92 <edoardo.bellone@inaf.it>
2025-04-09 17:25:47 +0200

    updating to new version of specula, still errors:ValueError: Input common_layer_list for object <specula.processing_objects.atmo_propagation.AtmoPropagation object at 0x00000242E5A09410> has not been set

--- bronte/mains/main250409_testing_specula_generated_phase_screens.py
+++ bronte/mains/main250409_testing_specula_generated_phase_screens.py
@@ -32,5 +32,5 @@ def kolmogorov_like_pahse_screens(ftag = '250409_090000'):
     
     psg = PhaseScreenGenerator(sf)
     psg.PROPAGATION_DIR = 'on_axis'
-    psg.run(30000)
+    psg.run(10)
     psg.save(ftag)
--- bronte/scao/phase_screen/phase_screen_generator.py
+++ bronte/scao/phase_screen/phase_screen_generator.py
@@ -5,14 +5,10 @@ from bronte.startup import set_data_dir
 from bronte.package_data import phase_screen_folder
 from arte.types.wavefront import Wavefront
 from astropy.io import fits 
-#import h5py
-#from arte.utils.circular_buffer import NumpyCircularBuffer
 
 class PhaseScreenGenerator():
     
     PROPAGATION_DIR = 'on_axis' #'lgs1'
-    #STORE_PHASE_SCREENS = True
-    BUFFER_SIZE = 100
     
     def __init__(self, scao_factory):
         
@@ -25,7 +21,6 @@ class PhaseScreenGenerator():
         self._define_groups()
         self._initialise_telemetry_buffers()
     
-
     def _setup_atmosphere(self):
         
         self._seeing = self._factory.seeing
@@ -40,12 +35,8 @@ class PhaseScreenGenerator():
         self._atmo.inputs['wind_speed'].set(self._wind_speed.output)
         self._prop.inputs['atmo_layer_list'].set(self._atmo.layer_list)
     
-        #self._bench_devices.inputs['ef'].set(self._prop.outputs['out_on_axis_source_ef'])
-    
     def _initialise_telemetry_buffers(self):
 
-        frame_shape = (self._slm_mask_diameter, self._slm_mask_diameter)
-        #self._phase_screen_buffer = NumpyCircularBuffer(self.BUFFER_SIZE, frame_shape, dtype=np.float16)
         self._modal_coefficients_list = []
     
     def _define_groups(self):
@@ -60,13 +51,7 @@ class PhaseScreenGenerator():
         ef_output = 'out_'+self.PROPAGATION_DIR+'_source_ef'
         ef = self._groups[2][0].outputs[ef_output]
         phase_screen = cpuArray(ef.phaseInNm)* 1e-9
-        
-        # if self.STORE_PHASE_SCREENS is True:
-        #     self._phase_screen_buffer.store(phase_screen)
-        #     if self._phase_screen_buffer.counter() == self._phase_screen_buffer._len:
-        #         self._save_phase_screens_hdf5(self._ftag_hdf5, self._phase_screen_buffer._buf )
-        #
-
+   
         phase_screen_on_slm_pupil = self._slm_raster.get_recentered_phase_screen_on_slm_pupil_frame(phase_screen)
         wfz = Wavefront.fromNumpyArray(phase_screen_on_slm_pupil)
         modal_coefficents = self._slm_raster._zernike_modal_decomposer.measureModalCoefficientsFromWavefront(
@@ -76,12 +61,8 @@ class PhaseScreenGenerator():
         
         self._modal_coefficients_list.append(modal_coefficents.toNumpyArray())
         
-    def run(self, Nsteps = 30):#, ftag_pahse_screen_hdf5 = None):
+    def run(self, Nsteps = 30):
         
-        # if ftag_pahse_screen_hdf5 is not None:
-        #     self.STORE_PHASE_SCREENS = True
-        #     self._ftag_hdf5 = ftag_pahse_screen_hdf5
-            
         self._n_steps = Nsteps
         # time step of the simulated loop isnt it QM
         self.time_step = self._factory.TIME_STEP_IN_SEC
@@ -89,7 +70,8 @@ class PhaseScreenGenerator():
         for group in self._groups:
             for obj in group:
                 obj.loop_dt = self.time_step * 1e9
-                obj.run_check(self.time_step)
+                #obj.run_check(self.time_step)
+                obj.setup(self.time_step, self._n_steps)
     
         for step in range(self._n_steps):
             t = 0 + step * self.time_step
@@ -134,35 +116,7 @@ class PhaseScreenGenerator():
         hdr['SLM_RAD'] = self._factory.SLM_PUPIL_RADIUS # in pixels
         hdr['SLM_YX'] = str(self._factory.SLM_PUPIL_CENTER) # YX pixel coordinates
         
-        fits.writeto(file_name, np.array(self._modal_coefficients_list), hdr)
-        
-        # if self.STORE_PHASE_SCREENS is True:
-        #     self._save_phase_screens_hdf5(ftag, np.array(self._phase_screen_list))
-    
-    #TODO: MemoryError must be resolved
-    # emoryError: Unable to allocate 12.0 GiB
-    # for an array with shape (5000, 1136, 1136) and data type float16
-    #TODO: the function must not overwrite but add the data on existing file
-    # def _save_phase_screens_hdf5(self, ftag, data):
-    #     frame_sizeY = self._slm_mask_diameter
-    #     frame_sizeX = self._slm_mask_diameter
-    #     file_name = phase_screen_folder() / (ftag + '.h5')
-    #     with h5py.File(file_name, 'w') as f:
-    #         # Create dataset with gzip compression and chunking
-    #         f.create_dataset(
-    #             'phase_screen', 
-    #             data=data.astype(np.float32), 
-    #             compression='gzip',
-    #             compression_opts = 9, 
-    #             chunks=(1, frame_sizeY, frame_sizeX)  # Save frame-wise chunks
-    #         )
-    #         # # Espandi dataset e salva
-    #         # f['phase_screen'].resize((f['phase_screen'].shape[0] + self._phase_screen_buffer.counter(),) + f['phase_screen'].shape[1:])
-    #         # f['phase_screen'][-self._phase_screen_buffer.counter():] = self._phase_screen_buffer._buf
-    #         # Svuota il buffer dopo il salvataggio
-    #         self._phase_screen_buffer = NumpyCircularBuffer(self.BUFFER_SIZE, (frame_sizeY, frame_sizeX), dtype=np.float32)
-    #
-
+        fits.writeto(file_name, np.array(self._modal_coefficients_list), hdr)
     @staticmethod
     def load_phase_screen_fits_data(ftag):
         set_data_dir()
@@ -172,11 +126,3 @@ class PhaseScreenGenerator():
         modal_coefficients_cube = hduList[0].data
         return  header, modal_coefficients_cube
     
-    # @staticmethod
-    # def load_phase_screen_hdf5(fname, frame_idx=None):
-    #     file_name = phase_screen_folder() / (fname + '.h5')
-    #     with h5py.File(file_name, 'r') as f:
-    #         if frame_idx is None:
-    #             return f['phase_screen'][:]  # Load full dataset
-    #         else:
-    #             return f['phase_screen'][frame_idx]  # Load specific frame
