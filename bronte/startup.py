d5586b45de432dbc5916cc50140d7e89ff08eb3d
idi92 <edoardo.bellone@inaf.it>
2025-04-09 14:26:35 +0200

    modified startup: added root

--- bronte/startup.py
+++ bronte/startup.py
@@ -4,6 +4,8 @@
 def set_data_dir():
     LB_ROOT = '/Users/lbusoni/Library/CloudStorage/GoogleDrive-lorenzo.busoni@inaf.it/.shortcut-targets-by-id/1SPpwbxlHyuuXmzaajup9lXpg_qSHjBX4/phd_slm_edo/misure_tesi_slm/bronte'
     EB_ROOT = "C:\\Users\\labot\\Desktop\\misure_tesi_slm\\bronte"
+    YM_ROOT = "C:\\Users\\edoar\\Desktop\\eximus\\phd_slm_edo\\bronte"
+    
     from bronte.package_data import set_data_root_dir
     set_data_root_dir(EB_ROOT)
     #set_data_root_dir(LB_ROOT)
